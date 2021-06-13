40919b43aaa3fea3d46bfe76b54e1cd49c4a1659
Catatomik <cata@catadev.ga>
2021-06-14 01:54:03 +0200

    :poop:

--- API/index.js
+++ API/index.js
@@ -3,16 +3,19 @@ const fs = require('fs');
 const https = require('https');
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/animals', function(err) {
-  if (err) {
-    throw err;
-  }
-});
-
 const config = require('../config.json').API
 const app = express()
 const router = require('./routes')()
 app.use(router)
+
+MongoClient.connect(config.database.url, {
+    options: {
+        auth: {
+            user: config.database.username,
+            password: config.database.password
+        }
+    }
+});
 
 const opts = {
   key: fs.readFileSync(config.key),
