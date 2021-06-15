8bb7edbee316c59cd2f0344366bc051b4b45d612
Catatomik <cata@catadev.ga>
2021-06-15 03:06:08 +0200

    :sparkles: Add middleware & working db connection

--- API/index.js
+++ API/index.js
@@ -5,8 +5,11 @@ const MongoClient = require('mongodb').MongoClient;
 
 const config = require('../config.json').API
 const app = express()
-const router = require('./routes')()
-app.use(router)
+app.use((req, res, next) => {
+    console.info(`[${(new Date()).toLocaleString("FR-fr")}] Treating request "${req.originalUrl}".`)
+    next()
+})
+require('./routes')(app)
 
 MongoClient.connect(config.database.url, {
     options: {
@@ -15,11 +18,18 @@ MongoClient.connect(config.database.url, {
             password: config.database.password
         }
     }
+}, (err, client) => {
+    if (err) throw err
+    app.client = client
+    app.db = client.db()
+    app.db.createIndex("urls", { shortened: "text" }, { unique: true })
+    console.log("Database connected.")
 });
 
 const opts = {
-  key: fs.readFileSync(config.key),
-  cert: fs.readFileSync(config.cert)
+   key: fs.readFileSync(config.key),
+   cert: fs.readFileSync(config.cert)
 };
 
 https.createServer(opts, app).listen(config.port);
+console.log(`Server started and listening on https://localhost:${config.port}.`)
