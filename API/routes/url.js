8bd15eedfde881ce87bf6bdc951ff17ea92b6186
Catatomik <cata@catadev.ga>
2021-06-17 19:18:14 +0200

    :bug::rocket: Deploy working password on API side

--- API/routes/url.js
+++ API/routes/url.js
@@ -5,11 +5,16 @@ module.exports = (app) => {
     
         .get(async (req, res, next) => {
 
-            if (!req.params?.password || req.params.password != app.config.password) return res.status(401).send({ status: 401, error: "Authentication error." })
+            if (!req.query?.password || req.query.password != app.config.password) {
+                res.status(401).send({ status: 401, error: "Authentication error." })
+                next()
+            } else {
 
-            const results = await app.db.collection('urls').find({}).toArray()
-            res.send(results)
-            next()
+                const results = await app.db.collection('urls').find({}).toArray()
+                res.send(results)
+                next()
+
+            }
         })
 
     router.route("/:url")
@@ -22,34 +27,43 @@ module.exports = (app) => {
 
         .post(async (req, res, next) => {
 
-            if (!req.body?.password || app.utils.sha256(req.body.password) != app.config.password) return res.status(401).send({ status: 401, error: "Authentication error." })
+            if (!req.body?.password || req.body.password != app.config.password) {
+                res.status(401).send({ status: 401, error: "Authentication error." })
+                next()
+            } else {
 
-            if (!req.body?.dest) return res.status(400).send({ status: 400, error: "No destination provided." })
-            try {
-                await app.db.collection('urls').insertOne({
-                    shortened: req.params.url,
-                    dest: req.body.dest
-                })
-                res.status(200).send({ status: 200 })
-            } catch(e) {
-                console.error(e)
-                res.status(500).send({ status: 500, error: e })
+                if (!req.body?.dest) return res.status(400).send({ status: 400, error: "No destination provided." })
+                try {
+                    await app.db.collection('urls').insertOne({
+                        shortened: req.params.url,
+                        dest: req.body.dest
+                    })
+                    res.status(200).send({ status: 200 })
+                } catch(e) {
+                    res.status(500).send({ status: 500, error: e })
+                }
+                next()
+
             }
-            next()
         })
 
         .delete(async (req, res, next) => {
 
-            if (!req.body?.password || app.utils.sha256(req.body.password) != app.config.password) return res.status(401).send({ status: 401, error: "Authentication error." })
+            if (!req.query?.password || req.query.password != app.config.password) {
+                res.status(401).send({ status: 401, error: "Authentication error." })
+                next()
+            } else {
 
-            try {
-                const r = await app.db.collection('urls').deleteOne({ shortened: req.params.url })
-                if (r.deletedCount == 0) throw new Error("No corresponding shortened URL found.")
-                res.status(200).send({ status: 200 })
-            } catch(e) {
-                res.status(500).send({ status: 500, error: e })
+                try {
+                    const r = await app.db.collection('urls').deleteOne({ shortened: req.params.url })
+                    if (r.deletedCount == 0) throw new Error("No corresponding shortened URL found.")
+                    res.status(200).send({ status: 200 })
+                } catch(e) {
+                    res.status(500).send({ status: 500, error: e })
+                }
+                next()
+
             }
-            next()
         })
         
     return {
